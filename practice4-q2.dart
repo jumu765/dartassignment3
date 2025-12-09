2246c0d3436995310fee9c632bf10df613bd9319
Jhuma(0182320012101199) <jhumasutradhar194@gmail.com>
2025-12-09 06:18:05 -0800

    Add files via upload

---
+++ practice4-q2.dart
@@ -0,0 +1,10 @@
+
+void main() {
+  Set<String> fruits = {"Apple", "Banana", "Mango", "Orange", "Grapes"};
+
+  print("List of Fruits:");
+
+  for (String fruit in fruits) {
+    print(fruit);
+  }
+}
