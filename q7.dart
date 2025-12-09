27819331d5871b75c5142a3c75d179623652479d
Jhuma(0182320012101199) <jhumasutradhar194@gmail.com>
2025-12-09 06:13:16 -0800

    Add files via upload

---
+++ q7.dart
@@ -0,0 +1,15 @@
+
+import 'dart:math';
+
+void main() {
+  int base = 5;
+  int exponent = 2;
+
+  num result = power(base, exponent);
+
+  print("$base^$exponent = $result");
+}
+
+num power(int base, int exponent) {
+  return pow(base, exponent);
+}
