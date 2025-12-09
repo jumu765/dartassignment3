153af5ea58c2b5e5617741b80e14f2097a2b99ab
Jhuma(0182320012101199) <jhumasutradhar194@gmail.com>
2025-12-09 06:14:04 -0800

    Add files via upload

---
+++ q8.dart
@@ -0,0 +1,13 @@
+
+void main() {
+  int a = 10;
+  int b = 20;
+
+  int result = add(a, b);
+
+  print("The sum of $a and $b is $result");
+}
+
+int add(int num1, int num2) {
+  return num1 + num2;
+}
