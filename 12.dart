567d9424d57a88e7fd5e383f06d787574645a579
Jhuma(0182320012101199) <jhumasutradhar194@gmail.com>
2025-12-09 06:17:07 -0800

    Add files via upload

---
+++ 12.dart
@@ -0,0 +1,10 @@
+
+void main() {
+  print(calculateArea(5, 3));  
+  print(calculateArea(7));     
+  print(calculateArea());      
+}
+
+double calculateArea([double length = 1, double width = 1]) {
+  return length * width;
+}
